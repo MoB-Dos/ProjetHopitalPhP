de0c17543f41ec3bf3fe13480112b08b1185e100
Enzo BIRBA <56584663+MoB-Dos@users.noreply.github.com>
2020-09-21 13:58:39 +0200

    Version 0

--- api paiement/FormuStreepUser.php
+++
@@ -1,7 +0,0 @@
-
-<form action="StreepUser.php" method="POST">
-</form>
-  <h3> Inscription Utilisateur </h3>
-  <input type="email" name="email"><br>
-  <input type="description" name="description">
-  <input type="submit" value="inscription" onclick="window.location='StreepUser.php';" />
