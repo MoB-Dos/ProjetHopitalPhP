a9d3d7defb67b19c3e349b960a042a732e542e05
Enzo BIRBA <56584663+MoB-Dos@users.noreply.github.com>
2020-10-27 10:59:57 +0100

    Arborescence2.1

--- hopitalphp.sql
+++ hopitalphp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 05 oct. 2020 à 13:12
+-- Généré le :  mar. 27 oct. 2020 à 09:58
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.2.18
 
@@ -93,17 +93,26 @@ CREATE TABLE IF NOT EXISTS `user` (
   `mail` varchar(35) NOT NULL,
   `token` text,
   `profil` varchar(9) NOT NULL,
+  `dossier` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `user`
 --
 
-INSERT INTO `user` (`id`, `login`, `mdpc`, `mdp`, `mail`, `token`, `profil`) VALUES
-(1, 'enzo', '', 'theoleboos', 'E.BIRBA@lprs.fr', NULL, 'medecin'),
-(2, 'test', '05a671c66aefea124cc08b76ea6d30bb', 'testtest', 'test@cheh.frsq', NULL, 'user'),
-(3, 'cc', 'c1f68ec06b490b3ecb4066b1b13a9ee9', 'cccccc', 'cc@cc.cc', NULL, 'admin');
+INSERT INTO `user` (`id`, `login`, `mdpc`, `mdp`, `mail`, `token`, `profil`, `dossier`) VALUES
+(1, 'enzo', '', 'theoleboos', 'E.BIRBA@lprs.fr', NULL, 'medecin', 0),
+(2, 'test', '05a671c66aefea124cc08b76ea6d30bb', 'testtest', 'test@cheh.frsq', NULL, 'user', 0),
+(3, 'cc', 'c1f68ec06b490b3ecb4066b1b13a9ee9', 'cccccc', 'cc@cc.cc', NULL, 'admin', 0),
+(4, 'j', 'ab4f63f9ac65152575886860dde480a1', 'azerty', 'j@j', NULL, 'user', 0),
+(5, 'k', '993e84021fe0f4f65a5f980257090177', 'Azertyu', 'k@k', NULL, 'user', 0),
+(6, 'f', '993e84021fe0f4f65a5f980257090177', 'Azertyu', 'f@f', NULL, 'user', 0),
+(7, 'r', '89df4f3cd604f5fccb2345ab329f327c', 'Azertyu1', 'r@r', NULL, 'user', 0),
+(11, 'u', 'd08d9ae829827c1b23cfe9d16baf7f3e', 'Azerty1', 'u@u', NULL, 'user', 0),
+(12, 'y', '89df4f3cd604f5fccb2345ab329f327c', 'Azertyu1', 'y@y', NULL, 'user', 1),
+(13, 'pol', '4f3876ccb80b752a6f558002c38ed066', 'Azertyui1', 'pol@gmail.com', NULL, 'user', 0),
+(14, 'hh', '89df4f3cd604f5fccb2345ab329f327c', 'Azertyu1', 'hh@gmail.com', NULL, 'user', 0);
 
 --
 -- Contraintes pour les tables déchargées
