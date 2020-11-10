e33e44498a71bf1535f3f2ac4385aef442968e81
Enzo BIRBA <56584663+MoB-Dos@users.noreply.github.com>
2020-11-10 16:08:49 +0100

    Affichage1.5

--- Hopital/ZoneTest/AjaxA.php
+++ Hopital/ZoneTest/AjaxA.php
@@ -1,9 +1,13 @@
 <?php
 
 
+echo '<script>';
+echo 'console.log('. console.debug( $_POST ) .')';
+echo '</script>';
+
 $tab= $_POST['tabUser'];
 
-$values = "'" . implode("', '", $_POST['tabUser']) . "'";
+//$values = "'" . implode("', '", $_POST['tabUser']) . "'";
 
 try
 {
@@ -16,7 +20,7 @@ catch(Exception $e)
 
 
 $req = $bdd->prepare("INSERT INTO tableau (nom,prenom,couleur) VALUES (?)");
-$req -> execute(array($values));
+$req -> execute(array($_POST['tabUser']['0']));
 
 
 
