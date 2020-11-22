7956dd50258d3920aa2d3365707500166917be04
Kikichaton <56584589+Kikichaton@users.noreply.github.com>
2020-11-22 12:22:36 +0100

    design formulaire mdp oublier

--- Hopital/View/Mdp/MdpOublier.php
+++ Hopital/View/Mdp/MdpOublier.php
@@ -1,12 +1,136 @@
-<form  action="../../Traitement/User/MdpOublierT.php" method="post">
+
+    <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Remedic - Free Bootstrap 4 Template by Colorlib</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	
+	
+    <?php  require_once('../../Tools/linkCSS.html') ?>
+    
+  	<!-- START nav -->
+  	<?php  require_once('../../Tools/NavBar.php') ?>
+    <!-- END nav -->
+
+  </head>
+  <body>
+
+  
+  <form  action="../../Traitement/User/MdpOublierT.php" method="post">
 
 
-            <input type="email" id="mail" name="mail" placeholder="Entrer votre mail" required /></br> </br>
+<input type="email" id="mail" name="mail" placeholder="Entrer votre mail" required /></br> </br>
 
-          <h3>cheh, fallait pas entrer un mail au hazard</h3>
 
-            <button class="primary-btn text-uppercase" onclick="window.location='MdpOublier2';">confirmation !</button>
+<button class="primary-btn text-uppercase" onclick="window.location='MdpOublier2';">confirmation !</button>
+
+</br> </br></br> </br></br> </br></br> </br></br> </br></br> </br></br> </br></br> </br></br> </br></br> </br>
+
+
+    
+  
+    
+    <input type="submit" value="retour"/><br><br>
 
     </form>
+    
 
-    <input type="submit" value="retour" onclick="window.location='../../index.php';" />
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--
+
+          <form action="Traitement/Dossier/dossierPatienT.php" method="post">
+          <div class="champ">
+          <section class="ftco-section-parallax">
+    <div class="container">
+      <div  class="row">
+            <img src="/Slam2/ProjethopitalPHP/Hopital/Design/images/doctor-1.jpg" width="400" height="500">
+				<div class="col-md-9 p-t-2">
+</div>
+</div>
+</div>
+
+
+
+              <div class="form-group">
+                <label for="appointment_name" class="text-black" name="prenom">Prénom</label>
+                <input type="text" class="form-control" id="prenom" name="prenom" style="width:550px; height:200px;">
+              </div>
+
+              <div class="form-group">
+                <label for="appointment_name" class="text-black" name="nom">Nom</label>
+                <input type="text" class="form-control" id="nom" name="nom"style="width:550px; height:200px;">
+              </div>
+
+              <div class="form-group">
+                <label for="appointment_name" class="text-black" name="date">Date de naissance</label>
+                <input type="Date" class="form-control" id="date" name="date"style="width:550px; height:200px;">
+              </div>
+
+              <div class="form-group">
+                <label for="appointment_name" class="text-black" name="adresse">Adresse</label>
+                <input type="text" class="form-control" id="adresse" name="adresse"style="width:550px; height:200px;">
+              </div>
+
+              <div class="form-group">
+                <label for="appointment_name" class="text-black" name="sq">"sq"</label>
+                <input type="text" class="form-control" id="sq" name="sq"style="width:550px; height:200px;">
+              </div>
+
+              <div class="form-group">
+                <label for="appointment_name" class="text-black" name="mutuel">"Mutuel"</label>
+                <input type="text" class="form-control" id="mutuel" name="mutuel"style="width:550px; height:200px;">
+              </div>
+
+              <div class="form-group">
+                <label for="appointment_name" class="text-black" name="OptionTV">Option TV</label>
+                <input type="text" class="form-control" id="optionTv" name="optionTv"style="width:550px; height:200px;">
+              </div>
+
+              <div class="form-group">
+                <label for="appointment_email" class="text-black" name="regime">Regime</label>
+                <input type="text" class="form-control" id="regime" name="regime"style="width:550px; height:200px;">
+              </div>
+          
+                
+              
+              <div class="form-group">
+                <input type="submit" value="Continuer" class="btn btn-primary">
+              </div>
+              
+            </form>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+
+--!>
+    
+  
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+	
+  <?php  require_once('../../Tools/linkJS.html') ?>
+    
+  </body>    
+  
+  <!--START footer-->
+    <?php  require_once('../../Tools/Footer.php') ?>
+  <!--END footer-->
+
+</html>
--- Hopital/View/Mdp/MdpOublier2.php
+++ Hopital/View/Mdp/MdpOublier2.php
@@ -1,17 +1,147 @@
 
+<html lang="en">
+  <head>
+    <title>Remedic - Free Bootstrap 4 Template by Colorlib</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	
+	
+    <?php  require_once('../../Tools/linkCSS.html') ?>
+    
+  	<!-- START nav -->
+  	<?php  require_once('../../Tools/NavBar.php') ?>
+    <!-- END nav -->
+
+  </head>
+  <body>
+
+  
 
 
-<form  action="../../Traitement/User/MdpOublierT2.php" method="post">
 
-      <input type="number" id="verif" name="verif" placeholder="Entrer votre code unique envoyer par mail" required /></br> </br>
+  <form  action="../../Traitement/User/MdpOublierT2.php" method="post">
 
-      <input type="password" id="mdp" name="mdp" placeholder="Entrer un Mot De Passe"  required /></br> </br>
+<input type="number" id="verif" name="verif" placeholder="Entrer votre code unique envoyer par mail" required /></br> </br>
 
-      <input type="password"  id="mdp" name="mdp2" placeholder="Retaper votre Mot De Passe" required /> </br> </br>
+<input type="password" id="mdp" name="mdp" placeholder="Entrer un Mot De Passe"  required /></br> </br>
+
+<input type="password"  id="mdp" name="mdp2" placeholder="Retaper votre Mot De Passe" required /> </br> </br>
 
 
 
-      <input type="submit" value="Confirmer"  />
-        </form>
+<input type="submit" value="Confirmer"  />
+  </form>
 
-        <input type="submit" value="retour" onclick="window.location='../../index.php';" />
+  <input type="submit" value="retour" onclick="window.location='../../index.php';" />
+
+  <!DOCTYPE html>
+
+</br> </br></br> </br></br> </br></br> </br></br> </br></br> </br></br> </br></br> </br></br> </br></br> </br>
+
+
+    
+  
+    
+    <input type="submit" value="retour"/><br><br>
+
+    </form>
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--
+
+          <form action="Traitement/Dossier/dossierPatienT.php" method="post">
+          <div class="champ">
+          <section class="ftco-section-parallax">
+    <div class="container">
+      <div  class="row">
+            <img src="/Slam2/ProjethopitalPHP/Hopital/Design/images/doctor-1.jpg" width="400" height="500">
+				<div class="col-md-9 p-t-2">
+</div>
+</div>
+</div>
+
+
+
+              <div class="form-group">
+                <label for="appointment_name" class="text-black" name="prenom">Prénom</label>
+                <input type="text" class="form-control" id="prenom" name="prenom" style="width:550px; height:200px;">
+              </div>
+
+              <div class="form-group">
+                <label for="appointment_name" class="text-black" name="nom">Nom</label>
+                <input type="text" class="form-control" id="nom" name="nom"style="width:550px; height:200px;">
+              </div>
+
+              <div class="form-group">
+                <label for="appointment_name" class="text-black" name="date">Date de naissance</label>
+                <input type="Date" class="form-control" id="date" name="date"style="width:550px; height:200px;">
+              </div>
+
+              <div class="form-group">
+                <label for="appointment_name" class="text-black" name="adresse">Adresse</label>
+                <input type="text" class="form-control" id="adresse" name="adresse"style="width:550px; height:200px;">
+              </div>
+
+              <div class="form-group">
+                <label for="appointment_name" class="text-black" name="sq">"sq"</label>
+                <input type="text" class="form-control" id="sq" name="sq"style="width:550px; height:200px;">
+              </div>
+
+              <div class="form-group">
+                <label for="appointment_name" class="text-black" name="mutuel">"Mutuel"</label>
+                <input type="text" class="form-control" id="mutuel" name="mutuel"style="width:550px; height:200px;">
+              </div>
+
+              <div class="form-group">
+                <label for="appointment_name" class="text-black" name="OptionTV">Option TV</label>
+                <input type="text" class="form-control" id="optionTv" name="optionTv"style="width:550px; height:200px;">
+              </div>
+
+              <div class="form-group">
+                <label for="appointment_email" class="text-black" name="regime">Regime</label>
+                <input type="text" class="form-control" id="regime" name="regime"style="width:550px; height:200px;">
+              </div>
+          
+                
+              
+              <div class="form-group">
+                <input type="submit" value="Continuer" class="btn btn-primary">
+              </div>
+              
+            </form>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+
+--!>
+    
+  
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+	
+  <?php  require_once('../../Tools/linkJS.html') ?>
+    
+  </body>    
+  
+  <!--START footer-->
+    <?php  require_once('../../Tools/Footer.php') ?>
+  <!--END footer-->
+
+</html>
